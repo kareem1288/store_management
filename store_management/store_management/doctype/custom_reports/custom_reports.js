d756ac04d9f8e875921ae8d4d645e97860ca7ac2
kareem <kareem@caratred.com>
2026-08-13 17:41:50 +0530

    Updated changes

---
+++ store_management/store_management/doctype/custom_reports/custom_reports.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Khaja Kareem Shaik and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Custom Reports", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ store_management/store_management/doctype/custom_reports/custom_reports.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Khaja Kareem Shaik and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CustomReports(Document):
+	pass
